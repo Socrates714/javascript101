af557092b42d7c22ff989897b64edbcd614ff93b
Troy <wolfwood7149@gmail.com>
2015-09-22 20:46:22 -0600

    Update primefactors.js

--- primefactors.js
+++ primefactors.js
@@ -1,20 +1,19 @@
-function largestprime (prime){  //calculates the largest prime factor
-	var factor = prime - 1;  //start at prime -1 and look for factor
+function largestprime (prime){  
+	var factor = prime - 1;  
 	while (prime % factor != 0) {
 		factor -= 1; 
 	}
-	if (factor > 1) {  //If the factor is more than one, the last
-					   //The last factor has not been reached
-		console.log("factor = " + factor); //shows current value of
-		console.log("prime = " + prime);   //factor and prime
-		return largestprime(factor); //Run function recursively to find
-									 //nexxt factor.
+	if (factor > 1) {  
+					   
+		console.log("factor = " + factor); 
+		console.log("prime = " + prime);   
+		return largestprime(factor);
 	}
 
-	else {  //if factor = 1 you have found the last prime.
-	console.log("At the end, factor = " + factor); //prints last value of
-	console.log("At the end, prime = " + prime);   //facotor and prime for logs
-	return prime; //return the final value of prime.
+	else {
+	console.log("At the end, factor = " + factor);
+	console.log("At the end, prime = " + prime);   
+	return prime; 
 	}
 }
 
